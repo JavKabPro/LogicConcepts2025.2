a972e9a897785ae276dadb2a75f9b17343034abd
JavKabPro <javierdelgado1139633@correo.itm.edu.co>
2025-08-20 11:23:53 -0500

    Is Leap year

---
+++ LogicConcepts/IsLeapYear/Program.cs
@@ -0,0 +1,42 @@
+﻿using Shared;
+do
+{
+    var currentYear = DateTime.Now.Year;
+    var message = string.Empty;
+    var year = ConsoleExtension.GetInt("Ingrese el año pa ver: ");
+    if (year == currentYear)
+    {
+        message = "es";
+    }
+    else if (year > currentYear)
+    {
+        message = "va a ser";
+    }
+    else
+    {
+        message = "fue";
+    }
+
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                Console.WriteLine($"{year} SI {message} bisiesto");
+            }
+            else
+            {
+                Console.WriteLine($"{year} NO {message} bisiesto");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"{year} SI {message} bisiesto");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"{year} NO {message} bisiesto");
+    }
+} while (true);
--- LogicConcepts/IsMultipleOfAnother/Program.cs
+++ LogicConcepts/IsMultipleOfAnother/Program.cs
@@ -3,12 +3,12 @@ do
 {
     var a = ConsoleExtension.GetInt("Ingrese el primer número: ");
     var b = ConsoleExtension.GetInt("Ingrese el segundo número: ");
-    if(b%a == 0)
+    if (b % a == 0)
     {
         Console.WriteLine($"{a} es múltiplo de {b}");
     }
     else
     {
-        Console.WriteLine($"{a} no es múltiplo de {b}");
+        Console.WriteLine($"{a} No es múltiplo de {b}");
     }
 } while (true);
