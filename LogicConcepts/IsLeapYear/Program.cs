c736da0c1bda8d719c06e0708b7e687373fb0aa6
JavKabPro <javierdelgado1139633@correo.itm.edu.co>
2025-08-22 17:51:28 -0500

    Improve exit promgrams

--- LogicConcepts/IsLeapYear/Program.cs
+++ LogicConcepts/IsLeapYear/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Shared;
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
 do
 {
     var currentYear = DateTime.Now.Year;
@@ -39,4 +41,11 @@ do
     {
         Console.WriteLine($"{year} NO {message} bisiesto");
     }
-} while (true);
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
--- LogicConcepts/IsMultipleOfAnother/Program.cs
+++ LogicConcepts/IsMultipleOfAnother/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Shared;
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
 do
 {
     var a = ConsoleExtension.GetInt("Ingrese el primer número: ");
@@ -11,4 +13,11 @@ do
     {
         Console.WriteLine($"{a} No es múltiplo de {b}");
     }
-} while (true);
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
